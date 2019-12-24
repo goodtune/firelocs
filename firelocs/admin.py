e7e558e1ac091242177748fad61218bcaa554c07
Gary Reynolds <gary.reynolds@optiver.com.au>
2019-12-24 11:05:00 +1100

    make admin more informative with list_display and list_filter

--- firelocs/admin.py
+++ firelocs/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 
 from firelocs.models import Incident
 
-admin.site.register(Incident)
+
+@admin.register(Incident)
+class IncidentAdmin(admin.ModelAdmin):
+    list_display = (
+        "location",
+        "fire_type",
+        "fire_status",
+        "alert_level",
+        "council_area",
+    )
+    list_filter = ("fire_status", "fire_type")
