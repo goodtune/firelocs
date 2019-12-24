08aaf5f3b67433bf8afc68b4d9e4e83e2ee3100d
Gary Reynolds <gary.reynolds@optiver.com.au>
2019-12-24 11:34:34 +1100

    Add firelocs.views.FireListJson. Closes #4.

--- firelocs/urls.py
+++ firelocs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from firelocs.views import FireList
+from firelocs.views import FireList, FireListJson
 
 urlpatterns = [
     path("", FireList.as_view()),
+    path("json/", FireListJson.as_view()),
 ]
--- firelocs/views.py
+++ firelocs/views.py
@@ -1,7 +1,17 @@
-from django.views.generic.list import ListView
+from django.http import JsonResponse
+from django.views.generic.base import View
+from django.views.generic.list import BaseListView, ListView
 
 from firelocs.models import Incident
 
 
 class FireList(ListView):
     model = Incident
+
+
+class FireListJson(BaseListView, View):
+    model = Incident
+
+    def render_to_response(self, context, **response_kwargs):
+        data = {each["id"]: each for each in self.get_queryset().values()}
+        return JsonResponse(data)
