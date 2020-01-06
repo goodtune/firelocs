18a5c179f07bad0b1a660606b70022e13c612278
Gary Reynolds <gary.reynolds@optiver.com.au>
2020-01-06 16:20:19 +1100

    Add a function-based view that can be extended to perform filtering.

--- firelocs/urls.py
+++ firelocs/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from firelocs.views import FireList, FireListJson
+from firelocs.views import FireList, FireListJson, drill_down_list
 
 urlpatterns = [
     path("", FireList.as_view()),
     path("json/", FireListJson.as_view()),
+    path("council/", drill_down_list),
+    path("council/<council_area>/", drill_down_list, name="council"),
 ]
--- firelocs/views.py
+++ firelocs/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.template.response import TemplateResponse
 from django.views.generic.base import View
 from django.views.generic.list import BaseListView, ListView
 
@@ -15,3 +16,12 @@ class FireListJson(BaseListView, View):
     def render_to_response(self, context, **response_kwargs):
         data = {each["id"]: each for each in self.get_queryset().values()}
         return JsonResponse(data)
+
+
+def drill_down_list(request, council_area=None):
+    object_list = Incident.objects.all()
+    context = {
+        "object_list": object_list,
+        "council_area": council_area,
+    }
+    return TemplateResponse(request, "firelocs/incident_drill.html", context)
